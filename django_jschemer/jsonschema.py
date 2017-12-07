518254d03c79c14baf1dac1d9083629783ffa209
Konstantinos Metaxas <arcanum@kill9.eu>
2017-12-07 12:43:41 +0200

    removed old, unused code and imports.

--- django_jschemer/jsonschema.py
+++ django_jschemer/jsonschema.py
@@ -1,16 +1,5 @@
-from django.forms import widgets, fields
 import inspect
 from .registry import registry
-
-
-def pretty_name(name):
-    """
-    Converts 'first_name' to 'First name'
-    """
-    if not name:
-        return ''
-        return name.replace('_', ' ').capitalize()
-
 
 class DjangoFormToJSONSchema(object):
 
@@ -55,52 +44,6 @@ class DjangoFormToJSONSchema(object):
         part = schemafield.get_schema_part()
         return part
 
-
-    def convert_formfield_old(self, name, field):
-        #TODO detect bound field
-        widget = field.widget
-        target_def = {
-            'title': str(field.label) or pretty_name(name),
-            'description': str(field.help_text),
-        }
-        # if field.required:                  #removed since it is not correct
-        #     target_def['required'] = [name] #TODO this likely is not correct
-        #TODO JSONSchemaField; include subschema and ref the type
-        if isinstance(field, fields.URLField):
-            target_def['type'] = 'string'
-            target_def['format'] = 'url'
-        elif isinstance(field, fields.FileField):
-            target_def['type'] = 'string'
-            target_def['format'] = 'uri'
-        elif isinstance(field, fields.DateField):
-            target_def['type'] = 'string'
-            target_def['format'] = 'date'
-        elif isinstance(field, fields.DateTimeField):
-            target_def['type'] = 'string'
-            target_def['format'] = 'datetime'
-        elif isinstance(field, (fields.DecimalField, fields.FloatField)):
-            target_def['type'] = 'number'
-        elif isinstance(field, fields.IntegerField):
-            target_def['type'] = 'integer'
-        elif isinstance(field, fields.EmailField):
-            target_def['type'] = 'string'
-            target_def['format'] = 'email'
-        elif isinstance(field, fields.NullBooleanField):
-            target_def['type'] = 'boolean'
-        elif isinstance(widget, widgets.CheckboxInput):
-            target_def['type'] = 'boolean'
-        elif isinstance(widget, widgets.Select):
-            if widget.allow_multiple_selected:
-                target_def['type'] = 'array'
-            else:
-                target_def['type'] = 'string'
-            target_def['enum'] = [choice[0] for choice in field.choices]
-        elif isinstance(widget, widgets.Input):
-            translated_type = self.input_type_map.get(widget.input_type, 'string')
-            target_def['type'] = translated_type
-        else:
-            target_def['type'] = 'string'
-        return target_def
 
 class DjangoModelToJSONSchema(DjangoFormToJSONSchema):
     def convert_model(self, model, json_schema=None):
